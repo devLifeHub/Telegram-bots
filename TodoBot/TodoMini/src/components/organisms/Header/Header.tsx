51a1531d504cef4493f4a402f24e872e63494bbd
Aleksandr <saskevich_aleksandr_tut_s@mail.ru>
2025-09-26 00:12:40 +0300

    WIP

--- TodoBot/TodoMini/src/components/organisms/Header/Header.tsx
+++ TodoBot/TodoMini/src/components/organisms/Header/Header.tsx
@@ -1,29 +1,13 @@
 import { FC } from "react"
 import Logo from "@/components/atoms/Logo/Logo";
-import LogoutIcon from "@/assets/icons/controls/logout.svg?react"
-
 import s from "./Header.module.scss"
 import clsx from "clsx";
-import { useLogoutMutation } from "@/api/services/auth";
 
 const Header: FC = () => {
-    const [ logout ] = useLogoutMutation()
-
-    const handleLogout = async () => {
-        try {
-            await logout().unwrap();
-        } catch (err) {
-            console.error("Error at logout:", err);
-        }
-    }
-
     return (
         <header className={s.header}>
             <div className={clsx(s.content, "container")}>
                 <Logo />
-                <button className={s.btn} onClick={handleLogout}>
-                    <LogoutIcon />
-                </button>
             </div>
         </header>
     )
