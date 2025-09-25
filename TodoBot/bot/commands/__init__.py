9c6f167a2866f61378aa1d4d2b15ace7ed67cf0d
Aleksandr <saskevich_aleksandr_tut_s@mail.ru>
2025-09-26 00:31:09 +0300

    feat: basic bot implementation completed

--- TodoBot/bot/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TODO_API_URL = os.getenv("TODO_API_URL", "http://server:8000")
--- TodoBot/bot/main.py
+++ TodoBot/bot/main.py
@@ -1,12 +1,17 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
-from bot.config import TELEGRAM_TOKEN
-from bot.commands.start import start
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler
+
+TOKEN = "7606175164:AAF3mIOhP9vTa4zrlW4hooFpbQ4jvjG62wQ"
+URL = "https://devlifehub.github.io/Telegram-bots/TodoMini/#/auth"
+
+async def start(update: Update, context):
+    kb = [[InlineKeyboardButton("Open Todo", web_app=WebAppInfo(url=URL))]]
+    await update.message.reply_text("WebApp", reply_markup=InlineKeyboardMarkup(kb))
+
 
 def main():
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
+    app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
